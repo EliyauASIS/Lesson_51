f4f4244dd41ea90faf192ee39425e5495699455c
EliyauASIS <eliyau987@gmail.com>
2023-11-08 20:05:28 +0200

    Eliyahu's Change 2

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,1 +1,17 @@
-alert: ""
+
+
+let itemsArr = []
+
+const Cart_Engine = (Name, Price) => {
+       let itemObj = {
+        name: Name,
+        price: Price
+       }
+       itemsArr.push(itemObj)
+       console.log(itemsArr);
+}
+
+const Cart = () => {
+   localStorage.setItem("CartItems",JSON.stringify(itemsArr))
+}
+
